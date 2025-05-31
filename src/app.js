7db9be806f7cf92d04372e279acdacb68e20ac2d
Amit Kumar <amit.kb.0123@gmail.com>
2025-05-31 11:21:32 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -3,28 +3,9 @@ const express = require('express');
 
 const app = express();
 
-app.use("/user",(req,res) => {
-    res.send("HAHAHAHA")
-}); 
-
-app.get("/user", (req,res) => {
+app.get("/user/:userId/:name/:password", (req,res) => {
+    console.log(req.params);
     res.send({firstName: "Amit", lastName: "Kumar"})
-});
-
-app.post("/user",(req,res) => {
-    // console.log("Save data to the database");
-    //Saving data
-    res.send("Data is successfully saved to database");
-
-});
-
-app.delete("/user", (req, res) => {
-    res.send("If you want to delete you can do.")
-});
-
-
-app.use("/test",(req,res) => {
-    res.send("Hello from the server");
 });
 
 

7ac32c053538bf513eac6ea8fb0677f42a619d4e
Amit Kumar <amit.kb.0123@gmail.com>
2025-05-31 10:56:45 +0530

    Exploring get,post delete

--- src/app.js
+++ src/app.js
@@ -3,17 +3,30 @@ const express = require('express');
 
 const app = express();
 
-app.use("/",(req,res) => {
-    res.send("Namaste bachoo");
+app.use("/user",(req,res) => {
+    res.send("HAHAHAHA")
+}); 
+
+app.get("/user", (req,res) => {
+    res.send({firstName: "Amit", lastName: "Kumar"})
 });
 
-app.use("/hello",(req,res) => {
-    res.send("Hello hello hello");
+app.post("/user",(req,res) => {
+    // console.log("Save data to the database");
+    //Saving data
+    res.send("Data is successfully saved to database");
+
 });
+
+app.delete("/user", (req, res) => {
+    res.send("If you want to delete you can do.")
+});
+
 
 app.use("/test",(req,res) => {
     res.send("Hello from the server");
 });
+
 
 app.listen(7777, ()=> {
     console.log("Server is running successfully on port 7777");

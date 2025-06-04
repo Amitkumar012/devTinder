0a86674b37db41689f048bc2ed3658c36ca2c228
Amit Kumar <amit.kb.0123@gmail.com>
2025-06-04 11:51:04 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -1,37 +1,19 @@
 
-
 const express = require('express');
 
 const app = express();
 
-app.post("/user/login",(req,res) => {
-    res.send("User logged in successfully");
-})
-
-const { adminAuth , userAuth } = require("./middlewares/auth"); 
-  // handle Auth Middleware for all GET,POST....
-app.use("/admin", adminAuth );
-app.use("/admin", userAuth );
-app.use("/user/data", adminAuth, (req,res) =>{
-    console.log("Data sent successfully" );
-    res.send("data sent")
-});
-
-app.options("/user/delete", adminAuth,(req,res) => {
-    res.send("Data deleted already")
-})
-
-app.get("/user", userAuth, (req,res) => { 
+app.get("/getUserData", (req,res) => { 
+    //Logic of DB call
+    throw new Error("dfgh");
     res.send("User data send");    
 }); 
 
-app.get("/admin/getAllData", (req,res) => { 
-    res.send("all data send");    
-});
-
-app.get("/admin/deleteUser", (req,res) => {
-        res.send("Deleted a user ");   
-});
+app.use("/", (err, req, res, next) =>{
+    if(err){
+        res.status(500).send("something went wrong")
+    }
+})
 
 app.listen(7777, () => {
     console.log("Server is running successfully on port 7777");
